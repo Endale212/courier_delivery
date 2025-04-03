1b0d4af0746a963a79047e9723f0bbb1743658be
Endale212 <endaleontop2112@gmail.com>
2025-04-03 16:25:16 +0300

    The courior delivery app first commit

---
+++ android/app/src/main/kotlin/com/example/we_courier_delivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package fast_time_express.delivery.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/fast_time_express/delivery/we_courier_delivery_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package fast_time_express.delivery.we_courier_delivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
